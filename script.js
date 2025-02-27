e542d2840f89b6bd96be4c8844d21e6623368bd6
Atikulla2001 <atikullahasib2001@gmail.com>
2025-02-27 14:02:23 +0600

    Responsivjsnavbar file update

---
+++ script.js
@@ -0,0 +1,7 @@
+let icon = document.querySelector(".phone_icon");
+
+let div = document.querySelector(".new_ul");
+
+icon.addEventListener(("click"),function () {
+    div.classList.toggle("open")
+})
